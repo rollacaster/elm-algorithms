53423ae4a7d8afa3f06f4a3f26d654ad55e59638
Thomas Sojka <thomas.sojka@comsysto.com>
2016-11-28 08:44:20 +0100

    🐛 Fix implemention of insertionSort

--- insertionSort.elm
+++ insertionSort.elm
@@ -3,20 +3,31 @@ module Main exposing (..)
 import Html exposing (Html)
 import Svg exposing (svg, rect)
 import Svg.Attributes exposing (height, width, x, fill, y)
+import Time
+import Debug exposing (log)
 
 
 main =
-    Html.beginnerProgram { model = model, view = view, update = update }
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
 
-model =
-    insertionSort [ 30, 20, 50, 70 ]
+init =
+    ( [ 40, 30, 60, 10, 40, 30, 60, 10, 40, 30, 60, 10 ], Cmd.none )
+
+
+type Msg
+    = UpdateArray Time.Time
 
 
 update msg model =
     case msg of
-        _ ->
-            model
+        UpdateArray _ ->
+            ( model, Cmd.none )
 
 
 view model =
@@ -24,7 +35,7 @@ view model =
         svgHeight =
             100
     in
-        svg [ height (toString svgHeight), width "100" ]
+        svg [ height (toString svgHeight), width "300" ]
             (List.indexedMap
                 (\index element ->
                     let
@@ -36,31 +47,48 @@ view model =
                     in
                         rect
                             [ y (toString (svgHeight - element))
-                            , fill "red"
+                            , fill ("#" ++ (toString (15 * element)))
                             , width (toString colWidth)
-                            , x (toString (index * colWidth + margin))
+                            , x (toString ((2 * index * colWidth) + margin))
                             , height (toString element)
                             ]
                             []
                 )
-                model
+                (log "model" (insertionSort [] [ 40, 30, 60, 10, 40, 30, 60, 10, 40, 30, 60, 10 ]))
             )
 
+insert = insertHelper []
 
-insertionSort =
-    insertionSortHelper []
-
-
-insertionSortHelper head list =
-    case list of
+insertHelper head element sorted =
+    case sorted of
         [] ->
-            []
+            [ element ]
 
         [ x ] ->
-            List.append head [ x ]
-
-        x :: (y :: xs) ->
-            if y > x then
-                insertionSortHelper (List.append head [ x ]) (y :: xs)
+            if x < element then
+                [ x, element ]
             else
-                insertionSortHelper (List.append head [ y ]) (x :: xs)
+                [ element, x ]
+
+        x :: xs ->
+            if x < element then
+                insertHelper (List.append head [x]) element xs
+            else
+                List.append head (element :: sorted)
+
+
+insertionSort sorted list =
+    case list of
+        [] ->
+            sorted
+
+        [ x ] ->
+            insert x sorted
+
+        x :: xs ->
+            insertionSort (insert x sorted) xs
+
+
+subscriptions model =
+    Sub.none
+    -- Time.every (Time.second / 10) UpdateArray
